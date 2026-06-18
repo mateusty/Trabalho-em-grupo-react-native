e145579209e8e538347a3ec9691c7cd45739d59a
mateusty <mateustamaki43@gmail.com>
2026-06-18 18:49:22 -0300

    Arrumando dependencias faltnado

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import { Routers } from './src/router';
 import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './src/context/AuthContext';
 
 export default function App() {
   return (
-    <AuthProvider>
       <NavigationContainer>
         <Routers />
       </NavigationContainer>
-    </AuthProvider>
   );
 }
 
