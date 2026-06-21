635cee346c62a6aca7efa6e64e825828bcdf7b96
pedromartins01 <pedropintomartinsdesouza@gmail.com>
2026-06-21 15:19:17 -0300

    Voltando código ao padrão que era antes de começar a fazer a home

--- src/router/stack.tsx
+++ src/router/stack.tsx
@@ -1,14 +1,19 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { LoginCadastro } from '../pages/LoginCadastro';
 import { DrawerRouter } from './drawer';
+import { useAuth } from '../context/AuthContext';
 
 const Stack = createStackNavigator();
 
 export function StackRouter() {
+  const { user } = useAuth();
+
+  const shouldRedirect = user && user.tipo_deficiencia && user.tipo_deficiencia !== '';
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={LoginCadastro} />
-      <Stack.Screen name="DrawerRouter" component={DrawerRouter} />
+
+        <Stack.Screen name="Drawer" component={DrawerRouter} />
+
     </Stack.Navigator>
   );
 }
