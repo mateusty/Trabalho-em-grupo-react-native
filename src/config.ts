b3f71ab1389bcefb9e9c5db1b48a3641f6b5813d
Yuri dos Santos Martins <112443954+Y-M-dev@users.noreply.github.com>
2026-06-22 17:11:28 -0300

    esse gitignore tá de sacanagem
    aaaaaaaaaaa

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = 'https://avqdtiupfebyeguoacfu.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_bUtdZaat7sPqcb63GpPv9w_VGqF8MRA';
-export const GOOGLE_WEB_CLIENT_ID = '1086856586899-8ccqqt7desb4jlsn1ebc7qd7iie9qhhp.apps.googleusercontent.com';
+export const SUPABASE_URL = '';
+export const SUPABASE_ANON_KEY = '';
+export const GOOGLE_WEB_CLIENT_ID = '';
